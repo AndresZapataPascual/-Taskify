70268572c34d595f63ed90e509081d8608d3e09c
AndresZapataPascual <andres.zapata019@pascualbravo.edu.co>
2024-05-06 20:23:02 -0500

    Inicio Y login
    Listos

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Tu código JavaScript aquí
+    document.getElementById('btnInicio').addEventListener('click', function() {
+        window.location.href = 'inicio.html';
+    });
+
+    document.getElementById('btnRegistro').addEventListener('click', function() {
+        window.location.href = 'registro.html';
+    });
+});
